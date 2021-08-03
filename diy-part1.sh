3bfdf9074a0290f88fe4147c3902c55d900939b3
wch32062 <86425947+wch32062@users.noreply.github.com>
2021-08-03 14:46:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git hellowrld https://github.com/fw876/helloworld' feeds.conf.default                
 © 2021 GitHub, Inc.
