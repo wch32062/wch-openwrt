7e8ea05cc61620cf1ffaf04e9f5ae75d3a1feda7
wch32062 <86425947+wch32062@users.noreply.github.com>
2021-08-03 14:51:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,10 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git hellowrld https://github.com/fw876/helloworld' feeds.conf.default                
-© 2021 GitHub, Inc.
+
