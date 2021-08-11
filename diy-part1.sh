86046c6e3e2b6122b9c51f7bc538601cc7419b53
wch32062 <86425947+wch32062@users.noreply.github.com>
2021-08-11 18:06:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.defaul
+echo 'src-git litte https://github.com/kenzok8/litte package/litte' >>feeds.conf.defaul
