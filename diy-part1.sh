9ffe8ebe402e2df25bff271b8cd3afe83cfa5b0a
wch32062 <86425947+wch32062@users.noreply.github.com>
2021-12-24 22:07:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,5 @@
 #
 #Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-echo 'src-git jeii https://github.com/kenzok78/jeii package/jeii' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
