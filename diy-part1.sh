557a92b9fef1b4432908bd9016908a48ae86cb6d
wch32062 <86425947+wch32062@users.noreply.github.com>
2023-05-12 21:19:35 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,11 +12,12 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git UA2F https://github.com/EOYOHOO/UA2F' >>feeds.conf.default
+#echo 'src-git rkpipid https://github.com/EOYOHOO/rkp-ipid' >>feeds.conf.default
+echo 'src-git campusNetwork https://github.com/FunctionXJB/Campus-network' >>feeds.conf.default
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
