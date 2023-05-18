b698b76c3c1e08f07454e8639051229fb16f22fe
wch32062 <86425947+wch32062@users.noreply.github.com>
2023-05-19 01:38:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$asrc-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #git clone maxminddb https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git  
-sed -i '$a src-git openwrt_kiddin9 https://op.supes.top/packages feeds.conf.default
+
