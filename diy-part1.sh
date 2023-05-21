af4f9c2b0c7f6e386929e5877f00e8d200cc2a1a
wch32062 <86425947+wch32062@users.noreply.github.com>
2023-05-21 21:48:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,8 @@
 #sed -i '$asrc-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #git clone maxminddb https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git  
-src-git openwrt https://git.openwrt.org/openwrt/openwrt.git >>feeds.conf.default
+sed -i '$a src-git openwrt https://git.openwrt.org/openwrt/openwrt' feeds.conf.default
+sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
+sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
+sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
