5351eebdb84ba3aa2ae70bf4e6e9569b84708268
wch32062 <86425947+wch32062@users.noreply.github.com>
2023-05-22 23:09:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git haiibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
+#echo 'src-git haiibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
