919d6b32ba53bd347df1275bb37823b022ee949a
wch32062 <86425947+wch32062@users.noreply.github.com>
2023-05-29 22:38:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@ echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git x-wrt https://github.com/x-wrt/packages' >>feeds.conf.default
+#echo 'src-git x-wrt https://github.com/x-wrt/packages' >>feeds.conf.default
