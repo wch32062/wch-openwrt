0975c64d07d7b0a3a6bd88bdfe8353798c3194c8
wch32062 <86425947+wch32062@users.noreply.github.com>
2023-05-29 22:29:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,9 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+#
 # Add a feed source
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git haiibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
