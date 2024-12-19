2ccbca57544c43137af680aa5aa234cda8c8867f
wch32062 <86425947+wch32062@users.noreply.github.com>
2024-12-19 20:52:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #
 # Add a feed source
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.defaul
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
